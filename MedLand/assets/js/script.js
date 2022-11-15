a792cce90e9c11b1e569f737f0273007d3cfea32
João Barbosa <contato.jsbarbosa@gmail.com>
2022-11-15 15:15:24 -0300

    Finalização do projeto

--- MedLand/assets/js/script.js
+++ MedLand/assets/js/script.js
@@ -1,4 +1,14 @@
-function myFunction(id) {
+window.onload = function () {
+  document.querySelector(".menu-mobile").addEventListener("click", function () {
+    if (document.querySelector(".menu nav ul").style.display == "flex") {
+      document.querySelector(".menu nav ul").style.display = "none";
+    } else {
+      document.querySelector(".menu nav ul").style.display = "flex";
+    }
+  });
+};
+
+function faq(id) {
   var x = document.getElementById(id);
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
